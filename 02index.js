1205c1df1cab91d78b688dda989fa96a89efbc23
Frany28 <franciely.vivas@gmail.com>
2024-06-10 17:38:25 -0400

    Initial commit

---
+++ 02index.js
@@ -0,0 +1,13 @@
+/*
+2) Crea una funcion que sirva como una calculadora y reciba dos numeros
+y una operacion y retorne el resultado y posteriormente imprima el resultado
+*/
+
+function calculo(resultado = 0) {
+  let operacion = prompt("Ingrese su operación matemática:");
+  resultado = eval(operacion);
+  return resultado;
+}
+
+resultado = calculo();
+alert("El resultado es " + resultado);
